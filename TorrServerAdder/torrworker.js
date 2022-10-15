6eb76bd1f12acb238d81c94e94f3c482c2d1c67b
Noperkot <92878142+Noperkot@users.noreply.github.com>
2022-10-15 20:15:34 +0300

    text alignment

--- TorrServerAdder/torrworker.js
+++ TorrServerAdder/torrworker.js
@@ -3,18 +3,18 @@ const addTorrent = {};
 const HIDE_DELAY = 15;	// время после начала воспроизведения, через которое должно быть закрыто окно прелоада
 
 class tWorkerSrv {
-    // #abortCtrl	// аборт-контроллер, единый для всей цепочки. используется для прерывания fetch
-    // #torrInfo	// информация собранная о торренте - magnet, url, название, постер и т.д.
+	// #abortCtrl	// аборт-контроллер, единый для всей цепочки. используется для прерывания fetch
+	// #torrInfo	// информация собранная о торренте - magnet, url, название, постер и т.д.
 	// #TS_address	// нормализованный адрес ТС
 	// #TS_headers	// заголовок для авторизации(если в url ТС заданы имя,пароль)
 	// #hash
 
-    constructor(msgPort) {
+	constructor(msgPort) {
 		this.msgPort = msgPort;	
-        this.msgPort.onMessage.addListener(this.onMessage);		
-        this.msgPort.onDisconnect.addListener(this.Abort);						// дисконнект происходит при вызове disconnect() в контент-скрипте, при уходе/обновлении/закрытии вкладки или по клику по окну предзагрузки
+		this.msgPort.onMessage.addListener(this.onMessage);		
+		this.msgPort.onDisconnect.addListener(this.Abort);						// дисконнект происходит при вызове disconnect() в контент-скрипте, при уходе/обновлении/закрытии вкладки или по клику по окну предзагрузки
 		this.timeoutTimer = setTimeout(this.Abort, 270000);						// таймаут на 30 сек меньше времени жизни сервис-воркера(5мин) = 4.5мин = 270сек
-    }
+	}
 	
 	onMessage = (request) => {		
 		switch (request.action) {
@@ -58,16 +58,16 @@ class tWorkerSrv {
 		} catch{}
 	}
 
-    Abort = () => {
+	Abort = () => {
 		this.abortCtrl.abort();
 		if(this.hash){
-            fetch(`${this.TS_address}/${this.path.drop}`, {
-                method: 'POST',
-                body: `{"action":"drop","hash":"${this.hash}"}`,				// в TS 1.1 "action" не нужен но и не мешает
-                headers: this.TS_headers,
-            }).catch((e) => {});
+			fetch(`${this.TS_address}/${this.path.drop}`, {
+				method: 'POST',
+				body: `{"action":"drop","hash":"${this.hash}"}`,				// в TS 1.1 "action" не нужен но и не мешает
+				headers: this.TS_headers,
+			}).catch((e) => {});
 		}
-    }
+	}
 	
 	Disconnect = () => {
 		this.msgPort.onMessage.removeListener(this.onMessage);	
@@ -96,14 +96,14 @@ class tWorkerSrv {
 		});
 	}
 
-    tsVer() {
-        return new Promise((resolve, reject) => {
-            fetch(`${this.TS_address}/echo`, {
-                signal: this.abortCtrl.signal,
-                headers: this.TS_headers,
-            })
-            .then(async (response) => {
-                if (response.ok) {
+	tsVer() {
+		return new Promise((resolve, reject) => {
+			fetch(`${this.TS_address}/echo`, {
+				signal: this.abortCtrl.signal,
+				headers: this.TS_headers,
+			})
+			.then(async (response) => {
+				if (response.ok) {
 					const response_text = await response.text();
 					for (let ver in addTorrent) {
 						if (response_text.startsWith(ver)) {
@@ -114,7 +114,7 @@ class tWorkerSrv {
 					reject(new tsaError("Unsupported_version_of_TorrServer", text));					
 				}
 				else reject(new tsaError('request_rejected', `${response.status} ${response.statusText}`));
-            })
+			})
 			.catch((e) => reject(new tsaError('TorrServer_is_not_responding', this.TS_address )));
 /* 			.catch((e) => {
 
@@ -122,61 +122,61 @@ class tWorkerSrv {
 
 				reject(new tsaError('TorrServer_is_not_responding', e.message ));
 			}); */
-        });
-    }
+		});
+	}
 	
-    Post(path, body) {
-        return new Promise((resolve, reject) => {
-            const url = `${this.TS_address}/${path}`;
-            fetch(url, {
-                method: 'POST',
-                body: body,
-                signal: this.abortCtrl.signal,
-                headers: this.TS_headers,
-            })
-            .then((response) => {
-                if (response.ok) resolve(response.text());
+	Post(path, body) {
+		return new Promise((resolve, reject) => {
+			const url = `${this.TS_address}/${path}`;
+			fetch(url, {
+				method: 'POST',
+				body: body,
+				signal: this.abortCtrl.signal,
+				headers: this.TS_headers,
+			})
+			.then((response) => {
+				if (response.ok) resolve(response.text());
 				else reject(new tsaError('request_rejected', `${response.status} ${response.statusText}`));
-            })
+			})
 			.catch((e) => reject(new tsaError('TorrServer_is_not_responding',this.TS_address)));
-        });
-    }
+		});
+	}
 
-    Preload() {
-        return new Promise((resolve, reject) => {
-            fetch(`${this.TS_address}/${this.path.preload}${this.hash}`, {
-                'method': 'HEAD',
-                'signal': this.abortCtrl.signal,
-                'headers': this.TS_headers,
-            })
-            .then((response) => {
-                if (response.ok) resolve();
+	Preload() {
+		return new Promise((resolve, reject) => {
+			fetch(`${this.TS_address}/${this.path.preload}${this.hash}`, {
+				'method': 'HEAD',
+				'signal': this.abortCtrl.signal,
+				'headers': this.TS_headers,
+			})
+			.then((response) => {
+				if (response.ok) resolve();
 				else reject(new tsaError('request_rejected', `${response.status} ${response.statusText}`));//
-            })
+			})
 			.catch((e) => reject((e.name === 'AbortError') ? new tsaError('timeout') : new tsaError('TorrServer_is_not_responding',this.TS_address)));
-        });
-    }
+		});
+	}
 
-    Stat() {
-        return new Promise((resolve, reject) => {
+	Stat() {
+		return new Promise((resolve, reject) => {
 			this.Post(this.path.stat, `{"action":"get","hash":"${this.hash}"}`) // в TS 1.1 "action" не нужен но и не мешает
-            .then((response) => JSON.parse(response))
-            .then((jsn) => {
-                let stat = {};
-                for (const field in this.statFields) {
-                    stat[field] = jsn[this.statFields[field]] || 0;
-                }
+			.then((response) => JSON.parse(response))
+			.then((jsn) => {
+				let stat = {};
+				for (const field in this.statFields) {
+					stat[field] = jsn[this.statFields[field]] || 0;
+				}
 				try{
-                    let first_file_size = jsn[this.statFields['FileStats']][0][this.statFields['Length']];
-                    if (first_file_size < stat.PreloadSize) stat.PreloadSize = first_file_size;
+					let first_file_size = jsn[this.statFields['FileStats']][0][this.statFields['Length']];
+					if (first_file_size < stat.PreloadSize) stat.PreloadSize = first_file_size;
 				} catch {}
 				resolve(stat);
-            }) 
+			}) 
 			.catch(reject);
-        });
-    }
+		});
+	}
 
-    Play() {	// загрузчик плейлиста
+	Play() {	// загрузчик плейлиста
 		return new Promise((resolve, reject) => {
 			let url = `${this.TS_address}/${this.path.m3u}${this.hash}`;
 			if (isChrome()) { // Chrome (download playlist)
@@ -196,7 +196,7 @@ class tWorkerSrv {
 				});
 			}
 		});
-    }
+	}
 
 	Load() {	// загрузчик торрент-файла
 		return new Promise((resolve, reject) => {
